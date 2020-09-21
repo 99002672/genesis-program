f6cf28ed16d41d02c2c8653c7d908839f82782e4
99002672 <71620661+99002672@users.noreply.github.com>
2020-09-21 09:25:05 +0530

    Create mytime.h

---
+++ c++ assignment/set2/mytime/mytime.h
@@ -0,0 +1,26 @@
+#pragma once
+
+#include "gtest/gtest.h"
+class Mytime
+{
+private:
+	int hrs;
+	int minutes;
+	int sec;
+public:
+	Mytime();
+	Mytime(int, int, int);
+	Mytime(int, int);
+	Mytime operator + (const Mytime&);
+	Mytime operator - (const Mytime&);
+	Mytime operator + (int);
+	Mytime operator - (int);
+	void operator++ ();
+	void operator++ (int);
+	bool operator == (const Mytime&);
+	//Mytime& operator += ();
+	Mytime& operator += (const Mytime&);
+	bool operator < (const Mytime&);
+	bool operator > (const Mytime&);
+	void display();
+};
