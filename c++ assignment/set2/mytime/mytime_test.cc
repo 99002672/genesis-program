05b9015f3f201a71f3997962fb7cf7c680a36bb0
99002672 <71620661+99002672@users.noreply.github.com>
2020-09-21 09:25:37 +0530

    Add files via upload

---
+++ c++ assignment/set2/mytime/mytime_test.cc
@@ -0,0 +1,12 @@
+#include "mytime.h"
+
+TEST(a, b)
+{
+		Mytime a1;
+		Mytime b(0, 0, 0);
+		EXPECT_EQ(true, a1 == b);
+		a1++;
+		Mytime c(0, 1, 0);
+		a1.display();
+		EXPECT_EQ(false, a1 == c);
+}
