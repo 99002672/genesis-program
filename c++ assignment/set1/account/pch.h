281715a43ea2bc3b7a95468954c7aa7c47c49e03
99002672 <71620661+99002672@users.noreply.github.com>
2020-09-21 09:06:41 +0530

    Create pch.h

---
+++ c++ assignment/set1/account/pch.h
@@ -0,0 +1,18 @@
+#pragma once
+#include<string>
+#include "gtest/gtest.h"
+class Account {
+	int m_accNumber; // ideally to be of string type
+	std::string m_accName;
+	double m_balance;
+
+public:
+	Account();
+	Account(int, std::string, double);
+	Account(int, std::string);
+	//Account(const Account&);
+	void debit(double);
+	void credit(double);
+	double getBalance() const;
+	void display();
+};
