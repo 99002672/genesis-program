2e81d8f60ecbc8236ec99bff8c59dd4bccbb1336
99002672 <71620661+99002672@users.noreply.github.com>
2020-09-21 09:04:54 +0530

    Create box.h

---
+++ c++ assignment/set1/boxes/box.h
@@ -0,0 +1,17 @@
+#pragma once
+#include "gtest/gtest.h"
+class Box {
+	int m_length;
+	int m_breadth;
+	int m_height;
+public:
+	Box();
+	Box(int, int, int);
+	Box(int);
+	Box(const Box&);
+	int length();
+	int breadth();
+	int height();
+	double volume();
+	void display();
+};
