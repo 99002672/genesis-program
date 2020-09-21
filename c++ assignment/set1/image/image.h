1f07c24b73490f62e13a18f46a2eb17961efe680
99002672 <71620661+99002672@users.noreply.github.com>
2020-09-21 09:16:37 +0530

    Create image.h

---
+++ c++ assignment/set1/image/image.h
@@ -0,0 +1,17 @@
+#pragma once
+
+#include "gtest/gtest.h"
+class Image {
+	int m_x;
+	int m_y;
+	int m_width;
+	int m_height;
+public:
+	Image();
+	Image(int, int, int, int);
+	Image(const Image&);
+	void move(int, int, int, int);
+	void scale(int, int);
+	void resize(int, int);
+	void display();
+};
